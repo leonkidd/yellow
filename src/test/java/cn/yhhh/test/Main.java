18ea4f90bf15a8aa18b8e826508ce6b743102b00
Leon Kidd <304552328@qq.com>
2014-02-05 23:39:15 +0800

    H2 to 00 开发中

--- src/test/java/cn/yhhh/test/Main.java
+++ src/test/java/cn/yhhh/test/Main.java
@@ -1,15 +1,5 @@
 package cn.yhhh.test;
 
-import java.io.FileInputStream;
-import java.util.Iterator;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-
-import cn.heroes.jkit.utils.ExcelUtils;
-
 public class Main {
 
 	/**
@@ -17,24 +7,9 @@ public class Main {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		Workbook book = ExcelUtils.create(new FileInputStream("1.xlsx"));
-		int numberOfSheets = book.getNumberOfSheets();
-		
-		// 迭代Sheet
-		for(int i = 0; i < numberOfSheets; i++) {
-			Sheet sheet = book.getSheetAt(i);
-			
-			// 迭代Row
-			Iterator<Row> rows = sheet.rowIterator();
-			while(rows.hasNext()) {
-				Row row = rows.next();
-
-				Cell cell10 = row.getCell(10);
-				// 迭代Cell
-				Iterator<Cell> cells = row.cellIterator();
-			}
-		}
-		
+		String s = "H2";
+		String regex = "\\d\\w";
+		System.out.println();
 	}
 
 }
