a96b48a10b14ecae78f5f5894bfb0f13fbab5efe
Leon Kidd <304552328@qq.com>
2014-02-10 23:51:25 +0800

    Temp

--- src/test/java/cn/yhhh/test/Lab.java
+++ src/test/java/cn/yhhh/test/Lab.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import cn.heroes.yellow.core.Yellow;
 import cn.heroes.yellow.core.impl.TDYellow;
+import cn.heroes.yellow.filler.Filler;
 import cn.heroes.yellow.intercepter.TDIntercepter;
 import cn.heroes.yellow.parser.TDParser;
 import cn.heroes.yellow.parser.impl.Sheet0Parser;
@@ -12,6 +13,8 @@ public class Lab {
 
 	public static void main(String[] args) {
 		File source = new File("test/lab.xlsx");
+		File output = new File("test/importer.xls");
+		Filler filler = null;
 		TDParser parser = new Sheet0Parser();
 		TDIntercepter intercepter = new LabIntercepter();
 		Yellow yellow = new TDYellow(parser, intercepter, null);
