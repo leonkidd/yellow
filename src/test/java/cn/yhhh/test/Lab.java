aea38361a6d44a822a8c493ff3180f04d7ed6692
Leon Kidd <304552328@qq.com>
2014-02-10 23:23:08 +0800

    开始着手列表转换的使用测试。
    zdl

---
+++ src/test/java/cn/yhhh/test/Lab.java
@@ -0,0 +1,21 @@
+﻿package cn.yhhh.test;
+
+import java.io.File;
+
+import cn.heroes.yellow.core.Yellow;
+import cn.heroes.yellow.core.impl.TDYellow;
+import cn.heroes.yellow.intercepter.TDIntercepter;
+import cn.heroes.yellow.parser.TDParser;
+import cn.heroes.yellow.parser.impl.Sheet0Parser;
+
+public class Lab {
+
+	public static void main(String[] args) {
+		File source = new File("test/lab.xlsx");
+		TDParser parser = new Sheet0Parser();
+		TDIntercepter intercepter = new LabIntercepter();
+		Yellow yellow = new TDYellow(parser, intercepter, null);
+		yellow.yellow(source);
+		
+	}
+}
